78774f87c552d4e1d039de5e90a32a48e0042c46
AnYeRim <49059414+AnYeRim@users.noreply.github.com>
2021-12-25 19:36:52 +0900

    Create Auth Email UI
    이메일 인증 화면 구현

---
+++ app/src/main/java/com/example/chattingapp/AuthEmailActivity.java
@@ -0,0 +1,14 @@
+package com.example.chattingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AuthEmailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth_email);
+    }
+}
