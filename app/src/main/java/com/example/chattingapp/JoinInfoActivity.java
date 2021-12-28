ee19fff65933ef67dbe94eaabf403abdbddea820
AnYeRim <49059414+AnYeRim@users.noreply.github.com>
2021-12-28 22:06:09 +0900

    Update joinInfo Activity
    가입 정보 입력 기능 일부 구현.

--- app/src/main/java/com/example/chattingapp/JoinInfoActivity.java
+++ app/src/main/java/com/example/chattingapp/JoinInfoActivity.java
@@ -2,13 +2,45 @@ package com.example.chattingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Toast;
 
-public class JoinInfoActivity extends AppCompatActivity {
+import com.example.chattingapp.databinding.ActivityJoinInfoBinding;
+
+public class JoinInfoActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivityJoinInfoBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_join_info);
+
+        binding = ActivityJoinInfoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnOK.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnOK:
+                // 지금까지 모은 데이터 보관하며 이메일 인증 화면 띄우기.
+                // 자동 친구 추가 체크되었는지 확인해서 그것도 데이터 가지고 있어야 함
+                if(binding.edtNikname.length() > 0
+                        && binding.edtBirthDay.length() > 0
+                        && !binding.spnGender.getSelectedItem().toString().equals("성별")){
+                    Intent authEmail = new Intent(this, AuthEmailActivity.class);
+                    startActivity(authEmail);
+                }else {
+                    Toast.makeText(this,"정보를 모두 입력해주세요.", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
 }
