e4d791feb955b44a5c3fc63dc819608db650fa34
AnYeRim <49059414+AnYeRim@users.noreply.github.com>
2021-12-28 22:14:16 +0900

    Update AuthEmailActivity.java
    이메일 인증 일부 기능 구현

--- app/src/main/java/com/example/chattingapp/AuthEmailActivity.java
+++ app/src/main/java/com/example/chattingapp/AuthEmailActivity.java
@@ -3,12 +3,48 @@ package com.example.chattingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class AuthEmailActivity extends AppCompatActivity {
+import com.example.chattingapp.databinding.ActivityAuthEmailBinding;
+
+public class AuthEmailActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivityAuthEmailBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_auth_email);
+
+        binding = ActivityAuthEmailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.txtLater.setOnClickListener(this);
+        binding.btnSend.setOnClickListener(this);
+        binding.btnOK.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.txtLater:
+                // 서버로 회원가입 정보 넘겨서 추가하고 메인화면 띄우기
+                finish();
+                break;
+            case R.id.btnSend:
+                if(binding.chkTerm.isChecked() == true){
+                    // 입력한 이메일로 인증번호 보내기
+                    binding.linearSendMail.setVisibility(View.GONE);
+                    binding.linearAuthNum.setVisibility(View.VISIBLE);
+                }else {
+                    Toast.makeText(this,"개인정보 이용 동의해주세요", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.btnOK:
+                // 인증번호 입력된 값과 보낸 값이 같은지 비교하고 맞으면 회원가입 완료하기
+                finish();
+                break;
+        }
+
     }
 }
