b294c263e65e0591b50f4fe83751a8d7126d278e
AnYeRim <49059414+AnYeRim@users.noreply.github.com>
2022-01-19 20:56:58 +0900

    modify ui margin, padding etc..

--- app/src/main/java/com/example/chattingapp/View/Activity/InfoActivity.java
+++ app/src/main/java/com/example/chattingapp/View/Activity/InfoActivity.java
@@ -1,6 +1,9 @@
 package com.example.chattingapp.View.Activity;
 
+import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,5 +21,28 @@ public class InfoActivity extends AppCompatActivity {
         binding = ActivityInfoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        binding.imgBackground.setPadding(0,
+                getStatusBarHeight(this),
+                0,
+                0);
+
     }
+
+    public static int getStatusBarHeight(Context context) {
+        int screenSizeType = (context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK);
+        int statusbar = 0;
+
+        if(screenSizeType != Configuration.SCREENLAYOUT_SIZE_XLARGE) {
+            int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+
+            if (resourceId > 0) { statusbar = context.getResources().getDimensionPixelSize(resourceId);
+            }
+        }
+        return statusbar;
+    }
+
 }
