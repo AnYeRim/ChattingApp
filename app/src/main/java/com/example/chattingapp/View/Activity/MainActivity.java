dc7271fd7bc8b860b9fa0315dd4851099016f893
AnYeRim <49059414+AnYeRim@users.noreply.github.com>
2022-02-27 00:28:31 +0900

    Refactoring MainActivity
    MainActivity 리팩토링
    
    중복된 코드를 메소드로 묶자.
    가독성이 좋아진다.
    수정할 때 한 곳만 수정하면 된다.
    유닛 테스트하기도 좋다.

--- app/src/main/java/com/example/chattingapp/View/Activity/MainActivity.java
+++ app/src/main/java/com/example/chattingapp/View/Activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.chattingapp.View.Activity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,9 +15,9 @@ import com.example.chattingapp.View.Fragment.EtcFragment;
 import com.example.chattingapp.View.Fragment.FriendsFragment;
 import com.example.chattingapp.View.Fragment.RoomListFragment;
 import com.example.chattingapp.databinding.ActivityMainBinding;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, NavigationBarView.OnItemSelectedListener {
 
     private ActivityUtils activityUtils;
     private ActivityMainBinding binding;
@@ -36,16 +37,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         frg_chat = new RoomListFragment();
         frg_etc = new EtcFragment();
 
-        binding.titleBar.txtTitle.setText("친구");
-        binding.titleBar.btnAddChat.setVisibility(View.GONE);
-        binding.titleBar.btnAddFriends.setVisibility(View.VISIBLE);
-        getSupportFragmentManager().beginTransaction().replace(R.id.frg_container, frg_friends).commit();
-
         binding.titleBar.btnAddChat.setOnClickListener(this);
         binding.titleBar.btnAddFriends.setOnClickListener(this);
+        binding.bottomTap.setOnItemSelectedListener(this);
+    }
 
-        binding.bottomTap.setOnNavigationItemSelectedListener(new ItemSelectedListener());
+    @Override
+    protected void onResume() {
+        super.onResume();
+        showSelectedItem(binding.bottomTap.getSelectedItemId());
+    }
 
+    private void showSelectedItem(int selectedItemId) {
+        switch (selectedItemId) {
+            case R.id.menu_friends:
+                setSelectedData("친구", binding.titleBar.btnAddFriends, frg_friends);
+                break;
+            case R.id.menu_chat:
+                setSelectedData("채팅", binding.titleBar.btnAddChat, frg_chat);
+                break;
+            case R.id.menu_etc:
+                setSelectedData("더보기", null, frg_etc);
+                break;
+        }
+    }
+
+    private void setSelectedData(String title, ImageView icon, Fragment fragment) {
+        setTitle(title);
+        showIcon(icon);
+        showFragment(fragment);
+    }
+
+    private void setTitle(String title) {
+        binding.titleBar.txtTitle.setText(title);
+    }
+
+    private void showIcon(ImageView icon) {
+        binding.titleBar.btnAddChat.setVisibility(View.GONE);
+        binding.titleBar.btnAddFriends.setVisibility(View.GONE);
+        if(icon != null){
+            icon.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void showFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.frg_container, fragment).commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        showSelectedItem(item.getItemId());
+        return true;
     }
 
     @Override
@@ -59,34 +101,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
-
-    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.menu_friends:
-                    binding.titleBar.txtTitle.setText("친구");
-                    binding.titleBar.btnAddChat.setVisibility(View.GONE);
-                    binding.titleBar.btnAddFriends.setVisibility(View.VISIBLE);
-                    getSupportFragmentManager().beginTransaction().replace(R.id.frg_container, frg_friends).commit();
-                    return true;
-
-                case R.id.menu_chat:
-                    binding.titleBar.txtTitle.setText("채팅");
-                    binding.titleBar.btnAddChat.setVisibility(View.VISIBLE);
-                    binding.titleBar.btnAddFriends.setVisibility(View.GONE);
-                    getSupportFragmentManager().beginTransaction().replace(R.id.frg_container, frg_chat).commit();
-                    return true;
-
-                case R.id.menu_etc:
-                    binding.titleBar.txtTitle.setText("더보기");
-                    binding.titleBar.btnAddChat.setVisibility(View.GONE);
-                    binding.titleBar.btnAddFriends.setVisibility(View.GONE);
-                    getSupportFragmentManager().beginTransaction().replace(R.id.frg_container, frg_etc).commit();
-                    return true;
-            }
-            return false;
-        }
-    }
-
 }
